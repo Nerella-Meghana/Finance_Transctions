370eed7284b649836e5ce38e8a8f5c27c55069e1
Meghana <meghananerella666@gmail.com>
2024-06-18 10:50:55 +0530

    Removed_Commented_code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/AuthService/Login_Page/LoginPage";
